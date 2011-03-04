a02b3f6ce4fe193c1064c215849d446e34bb2eae
Luwe <lieuwejan@gmail.com>
2011-03-04 12:36:58 +0100

    Added session adapter config

---
+++ config/session.php
@@ -0,0 +1,37 @@
+<?php defined('SYSPATH') or die('No direct access allowed.');
+/**
+ * Session adapter default for LJBase is 'database'. Use the following SQL
+ * query to create the table needed to run sessions under this adapter:
+ *
+ *      CREATE TABLE `session` (
+ *        `id` VARCHAR(24) NOT NULL,
+ *        `time` INT UNSIGNED NOT NULL,
+ *        `data` TEXT NOT NULL,
+ *        PRIMARY KEY (`id`),
+ *        INDEX (`time`)
+ *      ) ENGINE = MYISAM;
+ */  
+return array
+(
+  'native'   => array(
+    'name'     => 'session',
+    'lifetime' => 43200,
+  ),
+  'cookie'   => array(
+    'name'      => 'session',
+    'encrypted' => TRUE,
+    'lifetime'  => 43200,
+  ),
+  'database' => array(
+    'encrypted' => FALSE,
+    'lifetime'  => 43200,
+    'group'     => 'default',
+    'table'     => 'session',
+    'columns'   => array(
+      'session_id'  => 'id',
+      'last_active' => 'time',
+      'contents'    => 'data'
+    ),
+    'gc'        => 500,
+  ),
+);
